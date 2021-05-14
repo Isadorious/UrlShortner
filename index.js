a71ae3d208548f40ae42767a140476b9e7433cc0
Chris Groutage <chrisgroutage@gmail.com>
2021-05-14 14:11:25 +0100

    Setup file to run the app

---
+++ index.js
@@ -0,0 +1,28 @@
+const app = require('./express/app');
+const sequelize = require('./sequelize');
+
+require('dotenv').config();
+const PORT = process.env.PORT;
+
+async function assertDatabaseConnectionOk() {
+	console.log('Checking database connection...');
+	try {
+		await sequelize.authenticate();
+		console.log('Database connection OK!');
+	} catch (error) {
+		console.log('Unable to connect to the database:');
+		console.log(error.message);
+		process.exit(1);
+	}
+}
+
+async function init() {
+	await assertDatabaseConnectionOk();
+	console.log(`Starting link shortner on port ${PORT}...`);
+
+	app.listen(PORT, () => {
+		console.log(`Express server started on port ${PORT}.`);
+	});
+}
+
+init();
