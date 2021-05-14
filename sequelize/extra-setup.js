ab61ef197db239e0faf7027dc06d9a4377fe2521
Chris Groutage <chrisgroutage@gmail.com>
2021-05-14 10:32:36 +0100

    Setup foreign key on link

---
+++ sequelize/extra-setup.js
@@ -0,0 +1,12 @@
+function applyExtraSetup(sequelize) {
+	const {user, link} = sequelize.models;
+
+	// Link Foreign Keys
+	user.hasMany(link, {
+		foreignKey: {
+			name: 'creatorId',
+			allowNull: true,
+		}
+	});
+	link.belongsTo(user);
+}
